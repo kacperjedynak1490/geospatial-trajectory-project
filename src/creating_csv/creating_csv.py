38501438dcc522d6238643d5105b9539d2f2ea66
Kamil Kucia <kkucia2020@gmail.com>
2026-04-13 19:55:47 +0200

    separation of the datetime - KK

--- src/creating_csv/creating_csv.py
+++ src/creating_csv/creating_csv.py
@@ -4,21 +4,15 @@ import pandas as pd
 print("wczytywanie danych")
 df = pd.read_parquet('data/processed/taxi_prepared.parquet') 
 
-# datetime processing
-df['START_DATETIME'] = pd.to_datetime({
-    'year': df['YEAR'],
-    'month': df['MONTH'],
-    'day': df['DAY'],
-    'hour': df['HOUR'],
-    'minute': df['MINUTE'],
-    'second': df['SECOND']
-})
-
 # choosing columns for output
 wybrane_kolumny = [
     'TRIP_ID',          # Identyfikator trasy
-    'START_DATETIME',   # Data i godzina rozpoczęcia trasy
-    'ACTUAL_DIST_KM',   # Długość trasy w km
+    'YEAR',             # Rok
+    'MONTH',            # Miesiąc
+    'DAY',              # Dzień
+    'HOUR',             # Godzina
+    'MINUTE',           # Minuta
+    'SECOND',           # Sekunda    'ACTUAL_DIST_KM',   # Długość trasy w km
     'TRIP_TIME_MIN',    # Czas trasy w minutach
     'START_LON',        # Długość geo punktu startu
     'START_LAT'         # Szerokość geo punktu startu
