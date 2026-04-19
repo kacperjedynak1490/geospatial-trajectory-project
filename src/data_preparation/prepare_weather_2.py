e84578da5942a87eb74c6330a1ef98891f436451
Kamil Kucia <kkucia2020@gmail.com>
2026-04-19 16:47:18 +0200

    changing type to parquet-KK

--- src/data_preparation/prepare_weather_2.py
+++ src/data_preparation/prepare_weather_2.py
@@ -1,30 +1,31 @@
 import pandas as pd
 
 # loading data
-sciezka_wejsciowa = 'data/raw/weather/hourly_weather.parquet'
-print(f"Wczytywanie danych pogodowych z: {sciezka_wejsciowa}")
-df = pd.read_parquet(sciezka_wejsciowa)
+input_path = 'data/raw/weather/hourly_weather.parquet'
+print(f"Wczytywanie danych pogodowych z: {input_path}")
+df = pd.read_parquet(input_path)
 
 # rounding values to nearest integer
-kolumny_do_zaokraglenia = ['precipitation', 'wind_gusts_10m', 'is_day', 'weather_code', 'temperature_2m']
-df[kolumny_do_zaokraglenia] = df[kolumny_do_zaokraglenia].round()
+to_round = ['precipitation', 'wind_gusts_10m', 'is_day', 'weather_code', 'temperature_2m']
+df[to_round] = df[to_round].round()
 
 # choosing columns
-wybrane_kolumny = [
+choosed_columns = [
     'year', 'month', 'day', 'timezone', 'hour', 
     'precipitation', 'wind_gusts_10m', 'is_day', 'weather_code', 'temperature_2m'
 ]
-df = df[wybrane_kolumny]
+df = df[choosed_columns]
 
 # float to int
-df[kolumny_do_zaokraglenia] = df[kolumny_do_zaokraglenia].astype(int)
+df[to_round] = df[to_round].astype(int)
 
 # capital letters
 df.columns = [kolumna.upper() for kolumna in df.columns]
 
-# csv file output
-sciezka_wyjsciowa = 'data/raw/weather/weather.csv'
-print(f"saving new CSV file to: {sciezka_wyjsciowa}")
-df.to_csv(sciezka_wyjsciowa, index=False)
+# parquet file output
+output = 'data/processed/weather.parquet'
+print(f"file saved as: {output}")
 
-print("task completed successfully")
+df.to_parquet(output)
+
+print("task completed succesfully")
